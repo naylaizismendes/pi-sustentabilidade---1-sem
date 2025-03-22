480bc48cb4214697884c5bcae43143b13d8b987b
Rafael <you@example.com>
2025-03-21 21:59:18 -0300

    Feature: Adição das perguntas para armazenar os dados

--- verificacoes.py
+++ verificacoes.py
@@ -1,1 +1,14 @@
-print('TESTE')
+print('PROGRAMA PARA FAZER AS CONDICIONAIS')
+
+data = input('Digite a data de hoje: ')
+
+consumoAgua = int(input('Quantos litros de água você consumiu hoje? (Aprox em litros): '))
+
+consumoEnergia = int(input('Quantos kWh de energia elétrica você consumiu  hoje?: '))
+
+geracaoDeLixo = int(input('Quantos kgs de resíduos não recicláveis você gerou hoje?: '))
+
+porcentagemDeReciclagem = int(input('Qual a porcentagem de resíudos reciclados no total (em %)?: '))
+
+#A Fazer
+tipoTransporte = True
