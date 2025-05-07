8e9c38395dfd3f5848e363a34923973787d39e24
Vitória Mattos <vitoria.abm@puccampinas.edu.br>
2025-05-06 21:07:37 -0300

    Arrumando algumas funçoes

--- verificacoes.py
+++ verificacoes.py
@@ -1,4 +1,4 @@
-
+from copy import Error
 import mysql.connector
 
 #ao entrar no programa 
@@ -8,25 +8,25 @@ def primeiro_contato ():
    print("|     Sistema de Monitoramento de Sustentabilidade Pessoal      |")        
    print("|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|")
    print("|                                                               |")
-   print("| 1. Inserir dados de monitoramento                             |") # feito
-   print("| 2. Alterar dados de monitoramento                             |") #feito
-   print("| 3. Apagar dados de monitoramento                              |") #feito
-   print("| 4. Listar cada monitoramento diário e classificar             |") #feito
-   print("| 5. Calcular e mostrar as médias dos parâmetros e classificar  |") 
-   print("| 6. Sair do sistema                                            |") #feito
+   print("| 1. Inserir dados de monitoramento                             |") 
+   print("| 2. Alterar dados de monitoramento                             |") 
+   print("| 3. Apagar dados de monitoramento                              |") 
+   print("| 4. Listar cada monitoramento diário e classificar             |") #arrumar  
+   print("| 5. Calcular e mostrar as médias dos parâmetros e classificar  |") #terminar
+   print("| 6. Sair do sistema                                            |")
    print("|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|")
    print()
    
 
 #conexao  
-def obtemConexao (localhost, root, saopaulo, projeto_sustentabilidade):
-   if obtemConexao.conexao==None:
-      obtemConexao.conexao = mysql.connector.connect(host    =f"{localhost}",\
-                                                     user    =f"{root}",\
-                                                     password=f"{saopaulo}",\
-                                                     database=f"{projeto_sustentabilidade}")
+def obtemConexao (servidor, usuario, senha, bd):
+    if obtemConexao.conexao==None:
+        obtemConexao.conexao = mysql.connector.connect(host    =f"{servidor}",\
+                                                       user    =f"{usuario}",\
+                                                       password=f"{senha}",\
+                                                       database=f"{bd}")
 
-   return obtemConexao.conexao
+    return obtemConexao.conexao
 obtemConexao.conexao=None
    
 #inserir no banco       
@@ -38,7 +38,7 @@ def insercao_dados (data, qtd_de_agua_litros, uso_energia_eletria_kwh, residuos_
              "VALUES" +\
             f"('{data}', '{qtd_de_agua_litros}', '{uso_energia_eletria_kwh}', '{residuos_nao_reciclaveis_kg}', '{porcentagem_de_reciclado_hoje}','{transporte_publico}', '{bicicleta}', '{caminhada}', '{carro}', '{carro_eletrico}', '{carona}')"
 
-   conexao=obtemConexao("localhost","root","saopaulo","projeto_sustentabilidade") #arrumar
+   conexao=obtemConexao("localhost","root","saopaulo","projeto_sustentabilidade")
    cursor=conexao.cursor()
    cursor.execute(comando)
    conexao.commit()
@@ -122,84 +122,130 @@ def inserir ():
       print('Resposta inválida. Por favor, digite S ou N.')
       carona = input('6. Carona compartilhada (Fósseis): ').upper()
 
-   print("\n Dados inseridos com sucesso!\n")
+   insercao_dados(data, qtd_de_agua_litros, uso_energia_eletrica_kwh,
+            residuos_nao_reciclaveis_kg, porcentagem_de_reciclado_hoje,
+            transportePublico, bicicleta, caminhada, carro,
+            carroEletrico, carona)
+   
+   print("Dados inseridos com sucesso!")
 
 #atualizar uma informação
 def alterar ():
+   while True:
+      data_atualizar = input("Digite a data que deseja alterar (AAAA-MM-DD): ")
+      try:
+         data_atualizar = datetime.strptime(data_atualizar, "%d%m%Y").date()
+         break
+      except ValueError:
+         print("Data inválida. Por favor, insira no formato DDMMYYYY.")
+      
+   print(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ")
+   print("|                                                    |")
+   print("|                Menu de Atualização:                |")  
+   print("| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|")  
+   print("|                                                    |")    
+   print("| 1 - Atualizar água (L)                             |")              
+   print("| 2 - Atualizar energia eletrica (kwh)               |")
+   print("| 3 - Atualizar residuos não reciclados (kg)         |")
+   print("| 4 - Atualizar reciclados (%)                       |")
+   print("| 5 - Atualizar uso de transporte público (S/N)      |")
+   print("| 6 - Atualizar uso de caminhada (S/N)               |") 
+   print("| 7 - Atualizar uso de bicicleta (S/N)               |")
+   print("| 8 - Atualizar uso de carro (S/N)                   |")
+   print("| 9 - Atualizar uso de carro elétrico (S/N)          |")
+   print("| 10 - Atualizar uso de carona (S/N)                 |")
+   print("| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|")
+      
+      
+   dados = {
+      '1': 'qtd_de_agua_litros',
+      '2': 'uso_energia_eletrica_kwh',
+      '3': 'residuos_nao_reciclaveis_kg',
+      '4': 'porcentagem_de_reciclado_hoje',
+      '5': 'transporte_publico',
+      '6': 'caminhada',
+      '7': 'bicicleta',
+      '8': 'carro',
+      '9': 'carro_eletrico',
+      '10':'carona'
+   }
+   while True:
+      dados_opcao = input("Digite o número do campo que deseja atualizar: ")
+      if dados_opcao in dados:
+         break
+      else: 
+         print("Opção não existe; Tente novamente!")
     
-    data_atualizar = input("Digite a data que deseja alterar (AAAA-MM-DD): ")
-    #escolher oq quer mudar
-    print(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ")
-    print("|                                                    |")
-    print("|                Menu de Atualização:                |")  
-    print("| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|")  
-    print("|                                                    |")    
-    print("| 1 - Atualizar água (L)                             |")              
-    print("| 2 - Atualizar energia eletrica (kwh)               |")
-    print("| 3 - Atualizar residuos não reciclados (kg)         |")
-    print("| 4 - Atualizar reciclados (%)                       |")
-    print("| 5 - Atualizar uso de transporte público (S/N)      |")
-    print("| 6 - Atualizar uso de caminhada (S/N)               |") 
-    print("| 7 - Atualizar uso de bicicleta (S/N)               |")
-    print("| 8 - Atualizar uso de carro (S/N)                   |")
-    print("| 9 - Atualizar uso de carro elétrico (S/N)          |")
-    print("| 10 - Atualizar uso de carona (S/N)                 |")
-    print("| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|")
-    
-    
-    dados = {
-        '1': 'qtd_de_agua_litros',
-        '2': 'uso_energia_eletrica_kwh',
-        '3': 'residuos_nao_reciclaveis_kg',
-        '4': 'porcentagem_de_reciclado_hoje',
-        '5': 'transporte_publico',
-        '6': 'caminhada',
-        '7': 'bicicleta',
-        '8': 'carro',
-        '9': 'carro_eletrico',
-        '10':'carona'
-    }
-    while True:
-        dados_opcao = input("Digite o número do campo que deseja atualizar: ")
-        if dados_opcao in dados:
-            break
-        else: 
-            print("Opção não existe; Tente novamente!")
-    
-    novo_valor = input(f"Digite o novo valor para {dados[dados_opcao]}: ")
+   novo_valor = input(f"Digite o novo valor para {dados[dados_opcao]}: ")
 
-    comando = f"UPDATE verficador SET {dados} = %s WHERE data = %s"
-    conexao=obtemConexao("localhost","root","saopaulo","projeto_sustentabilidade") 
-    cursor=conexao.cursor()
-    cursor.execute(comando)
-    conexao.commit()
-    print("Atualização realizada com sucesso!")
-   
+   comando = f"UPDATE verficador SET {dados[dados_opcao]} = '{novo_valor}' WHERE data = '{data_atualizar}'"
+
+   try:
+      conexao=obtemConexao("localhost","root","saopaulo","projeto_sustentabilidade")
+      cursor = conexao.cursor()
+      cursor.execute(comando)
+      conexao.commit()
+      print("Informação atualizada com sucesso.")
+   except Error as e:
+      print(f"Erro ao atualizar: {e}")
    
 def excluir ():
-   confirmacao = input('Tem certeza que deseja excluir o ultimo dado cadastrado? ').upper()
+   confirmacao = input('Tem certeza que deseja excluir um registro? ').upper()
    while confirmacao not in ('S', 'N'):
       print('Resposta inválida. Por favor, digite S ou N.')
-      carona = input('Tem certeza que deseja excluir o ultimo dado cadastrado? ').upper()
+      confirmacao = input('Tem certeza que deseja excluir um registro? ').upper()
    
    if confirmacao == 'S':
-      data_excluir = input("Digite a data que deseja deletar: ")
-      comando = "DELETE FROM verficador WHERE data = %s"
+      while True:
+         data_excluir = input("Digite a data que deseja deletar: ")
+         try:
+            data_excluir = datetime.strptime(data_excluir, "%d%m%Y").date()
+            break
+         except ValueError:
+            print("Data inválida. Por favor, insira no formato DDMMYYYY.")
+
+      comando = f"DELETE FROM verficador WHERE data = '{data_excluir}'"
       conexao=obtemConexao("localhost","root","saopaulo","projeto_sustentabilidade")
       cursor=conexao.cursor()
       cursor.execute(comando)
+      conexao.commit()
       print("Registro deletado com sucesso!\n")
-   else :
-      print('Sistema encerrado!')
 
-#lista todos os registros       
-def listar ():
-   comando=("SELECT * FROM verficador")
-   conexao=obtemConexao("localhost","root","saopaulo","projeto_sustentabilidade")
-   cursor=conexao.cursor()
-   cursor.execute(comando)
-   linhas=cursor.fetchall()
-   return linhas
+def listagem ():
+    comando= "Select DATE_FORMAT(data,'%d/%m/%Y'), qtd_de_agua_litros, uso_energia_eletrica_kwh, residuos_nao_reciclaveis_kg, porcentagem_de_reciclado_hoje, transporte_publico, bicicleta, caminhada, carro,  carro_eletrico, carona FROM verficador"
+    conexao=obtemConexao("localhost","root","saopaulo","projeto_sustentabilidade")
+    cursor=conexao.cursor()
+    cursor.execute(comando)
+
+    linhas=cursor.fetchall()
+    return linhas
+
+#lista todos os registros  
+def listar (): #tá em loop??????
+    try:
+        linha=listagem()
+    except Error:    
+        print("Problema de conexão com o BD!")
+    else:
+        atual=0
+        while atual<len(linha):
+            print()
+            print('Data.......:',linha[atual][0])
+            print('Qtd de água:',linha[atual][1])
+            print('Qtd energia:',linha[atual][2])
+            print('Residuos não reciclaveis:',linha[atual][3])
+            print('reciclados :',linha[atual][4])
+            print('Transporte publico:',linha[atual][5])
+            print('bicicleta:',linha[atual][6])
+            print('caminhada:',linha[atual][7])
+            print('carro:',linha[atual][8])
+            print('carro eletrico:',linha[atual][9])
+            print('carona:',linha[atual][10])
+            atual+=1
+
+        print()
+        print("Listagem concluida com sucesso!")
+     
 
 def medias (): 
    conexao = obtemConexao("localhost", "root", "saopaulo", "projeto_sustentabilidade")
@@ -276,10 +322,20 @@ def fechaConexao ():
     cursor=conexao.cursor()
     cursor.close()
     conexao.close()
-    print('Saindo do sistema...')
-
+   
 primeiro_contato()
-opcao = input("Digite o número da opção desejada: ")
+
+digitou_corretamente = False
+while not digitou_corretamente:
+   try:
+      opcao = int(input("Digite o número da opção desejada: "))
+   except ValueError:
+      print("Digite apenas números; tente novamente!")
+   else:
+      if opcao <= 0 or opcao > 6:
+         print("Não existe essa opção; tente novamente!")
+      else:
+         digitou_corretamente = True
 
 desejaSairDoPrograma=False
 while not desejaSairDoPrograma:
@@ -297,3 +353,6 @@ while not desejaSairDoPrograma:
     else: 
         fechaConexao()
         desejaSairDoPrograma=True
+
+print()        
+print('PROGRAMA ENCERRADO; OBRIGADO POR USAR ESTE SISTEMA DE MONITORAMENTO PESSOAL DE SUSTENTABILIDADE!')
